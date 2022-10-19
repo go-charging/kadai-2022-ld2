ff0c87fadbc4e75a693e5f279704111e3cfb5e02
mojarinbaby <tinman.system01@gmail.com>
2022-10-19 04:50:15 +0000

    課題2を提出

---
+++ kadai-02/index.js
@@ -0,0 +1,15 @@
+const firstName = "Aiu"
+const familyName = "Eo"
+const fullName = `${firstName} ${familyName}`
+console.log(fullName)
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+console.log((1+10)*10/2)
+
+const dog = true
+const cat = false
+
+console.log ("犬または猫を飼っている", dog || cat)
+console.log ("犬と猫の両方を飼っている", dog && cat)
+console.log("犬も猫も飼っていない",!dog && !cat)
+console.log("犬も猫も飼っていない",!(dog || cat))
